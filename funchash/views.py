f553db4122d9c6e5478c4d5bdc2ea0107d0c2312
Yan Sardinha <yansardinha1001@gmail.com>
2023-08-28 08:55:57 -0300

    <FIX> Utilizando função hash ao invés de RSA

--- funchash/views.py
+++ funchash/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from .models import Chaves
+import hashlib
+import os
+
+def gera_chaves_hash():
+    seed = os.urandom(16) 
+    chave_privada = hashlib.sha256(seed).digest()
+    chave_publica = hashlib.sha256(chave_privada).digest()
+
+    return {
+        'chave_publica': chave_publica.hex(),
+        'chave_privada': chave_privada.hex()
+    }
 
 @login_required
 def gerar_chaves(request):
@@ -8,8 +20,9 @@ def gerar_chaves(request):
     chave_obj, created = Chaves.objects.get_or_create(user=user)
 
     if not chave_obj.chave_publica or not chave_obj.chave_privada:
-        chave_obj.chave_publica = "sua_chave_publica_aqui"
-        chave_obj.chave_privada = "sua_chave_privada_aqui"
+        chaves = gera_chaves_hash()
+        chave_obj.chave_publica = chaves['chave_publica']
+        chave_obj.chave_privada = chaves['chave_privada']
         chave_obj.save()
 
     return render(
