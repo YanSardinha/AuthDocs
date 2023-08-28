3bea3f6bdccdd17938afdc20eae9bafe45c602bd
Yan Sardinha <yansardinha1001@gmail.com>
2023-08-28 09:20:34 -0300

    <FIX> Criando seed única por usuário

--- funchash/views.py
+++ funchash/views.py
@@ -4,8 +4,8 @@ from .models import Chaves
 import hashlib
 import os
 
-def gera_chaves_hash():
-    seed = os.urandom(16) 
+
+def gera_chaves_hash(seed):
     chave_privada = hashlib.sha256(seed).digest()
     chave_publica = hashlib.sha256(chave_privada).digest()
 
@@ -14,13 +14,16 @@ def gera_chaves_hash():
         'chave_privada': chave_privada.hex()
     }
 
+
 @login_required
 def gerar_chaves(request):
     user = request.user
     chave_obj, created = Chaves.objects.get_or_create(user=user)
 
     if not chave_obj.chave_publica or not chave_obj.chave_privada:
-        chaves = gera_chaves_hash()
+        seed = f"{user.username}{user.email}".encode('utf-8')
+
+        chaves = gera_chaves_hash(seed)
         chave_obj.chave_publica = chaves['chave_publica']
         chave_obj.chave_privada = chaves['chave_privada']
         chave_obj.save()
