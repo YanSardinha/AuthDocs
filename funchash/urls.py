24aaa5b0899620322fa40d036780f68a5d3c040b
YanSardinha <yansardinha1001@gmail.com>
2023-08-30 17:49:10 -0300

    <FEAT> Gerando chave publica e privada na tela inicio

--- funchash/urls.py
+++ funchash/urls.py
@@ -1,11 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import valida_assinatura_publica, gerar_chaves, assinar_documento, anexar_documento, validar_assinatura, index, inicio, criar_mensagem, lista_mensagens, validar_mensagem, lista_documentos
+from .views import valida_assinatura_publica, assinar_documento, anexar_documento, validar_assinatura, index, inicio, criar_mensagem, lista_mensagens, validar_mensagem, lista_documentos
 
 urlpatterns = [
     path('', index, name="index"),
     path('inicio/', inicio, name="inicio"),
-    path('gerar_chave/', gerar_chaves, name="gerar_chaves"),
     path('assinar_documento/', assinar_documento, name="assinar_documento"),
     path('anexar_documento/', anexar_documento, name='anexar_documento'),
     path('criar_mensagem/', criar_mensagem, name='criar_mensagem'),
