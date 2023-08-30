11e91e5e8a677dc8cf67ec4466d879027160897d
Yan Sardinha <yansardinha1001@gmail.com>
2023-08-30 13:44:42 -0300

    <FIX>Pagina Inicio

--- funchash/admin.py
+++ funchash/admin.py
@@ -1,13 +1,18 @@
 from django.contrib import admin
-from .models import Chaves, Documento
+from .models import Chaves, Documento, Mensagem
 
-
+@admin.register(Chaves)
 class ChavesAdmin(admin.ModelAdmin):
     list_display = ('user', 'chave_publica', 'chave_privada')
+    list_filter = list_display
 
-admin.site.register(Chaves, ChavesAdmin)
 
 @admin.register(Documento)
 class DocumentoAdmin(admin.ModelAdmin):
     list_display = ('id','usuario', 'nome', 'assinatura', 'conteudo_hash', 'data_anexo','data_assinatura',)
     list_filter = ('usuario', 'nome', 'data_anexo', 'data_assinatura')
+
+@admin.register(Mensagem)
+class MensagemAdmin(admin.ModelAdmin):
+    list_display = ('conteudo', 'assinatura')
+    list_filter = list_display
